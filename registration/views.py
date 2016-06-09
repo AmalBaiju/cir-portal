a4c561bdce7e3c1ca7e4582c1526ea7b995a4dd3
AmalBaiju <amalbaiju21@gmail.com>
2016-06-09 15:05:00 +0530

    added filter features

--- registration/views.py
+++ registration/views.py
@@ -90,11 +90,11 @@ def handle_student_upload(request):
                 counter = counter+1
 
             return render_to_response('register/cirstaff/register_bulk_student_list.html',{'counter':counter },
-                                       context_instance=RequestContext(request))
+                                      context_instance=RequestContext(request))
         else :
-             return redirect(request.META['HTTP_REFERER'])
+            return redirect(request.META['HTTP_REFERER'])
     else:
-         return HttpResponseBadRequest()
+        return HttpResponseBadRequest()
 
 
 class StudentListView(LoginRequiredMixin,ListView):
@@ -106,7 +106,7 @@ class StudentListView(LoginRequiredMixin,ListView):
 
 class StudentListUpdateView(LoginRequiredMixin, UpdateView):
     model = Student
-    form_class = StudentRegistrationForm
+    fields = student_fields
     template_name_suffix = '_update_form'
     success_url = '/register/cirstaff/success/'
 
@@ -116,3 +116,16 @@ class StudentListUpdateView(LoginRequiredMixin, UpdateView):
             return obj
         else:
             raise Http404("That doesnt exist.")
+
+class StudentFilterExternalView(ListView):
+    template_name = 'register/cirstaff/filter_external_list.html'
+
+    def get_queryset(self):
+        cgpa = self.request.GET.get('cgpa')
+        arrears = self.request.GET.get('arrear')
+        branch = self.request.GET.get('branch')
+        tenth = self.request.GET.get('tenth')
+        twelth = self.request.GET.get('twelth')
+        print( cgpa + arrears + branch + tenth + twelth )
+        return Student.Objects.filter(cgpa__gte = cgpa,curr_arrears=arrears,branch=branch,
+                                      tenth_mark__gte=tenth, twelth_mark__gte=twelth)
